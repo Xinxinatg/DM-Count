9455bbb0efd044e9aa6624b7b518277603e9eda7
Xinxin Peng <72983482+Xinxinatg@users.noreply.github.com>
2020-12-23 01:01:05 +1100

    Update train.py

--- train.py
+++ train.py
@@ -61,8 +61,8 @@ def parse_args():
     if args.counter_type == 'cc':
        shutil.copytree(os.path.join(ori_dataset_dir,'train_data','downsampled-cropped-images'),os.path.join(ori_dataset_dir,args.counter_type,'train_data','images'))
        shutil.copytree(os.path.join(ori_dataset_dir,'train_data','downsampled-cropped-ground-truth'),os.path.join(ori_dataset_dir,args.counter_type,'train_data','ground-truth'))
-       shutil.copytree(os.path.join(ori_dataset_dir,test_data,'downsampled-cropped-ground-truth'),os.path.join(ori_dataset_dir,args.counter_type,'test_data','ground-truth'))
-       shutil.copytree(os.path.join(ori_dataset_dir,test_data,'downsampled-cropped-images'),os.path.join(ori_dataset_dir,args.counter_type,'test_data','images'))
+       shutil.copytree(os.path.join(ori_dataset_dir,'test_data','downsampled-cropped-ground-truth'),os.path.join(ori_dataset_dir,args.counter_type,'test_data','ground-truth'))
+       shutil.copytree(os.path.join(ori_dataset_dir,'test_data','downsampled-cropped-images'),os.path.join(ori_dataset_dir,args.counter_type,'test_data','images'))
        args.crop_size = 160
        args.dataset_dir= os.path.join(ori_dataset_dir,args.counter_type)
     elif args.counter_type == 'fc':
