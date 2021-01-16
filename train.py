e75300055134283a3a1181e33847e1fa09e51cc8
Xinxin Peng <72983482+Xinxinatg@users.noreply.github.com>
2021-01-17 00:34:30 +1100

    Update train.py

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ def parse_args():
                         help='the weight decay')
     parser.add_argument('--resume', default='', type=str,
                         help='the path of resume training model')
-    parser.add_argument('--max-epoch', type=int, default=75,
+    parser.add_argument('--max-epoch', type=int, default=250,
                         help='max training epoch')
     parser.add_argument('--val-epoch', type=int, default=5,
                         help='the num of steps to log training information')
