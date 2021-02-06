c3f173cc911cfd99a95717e6716f5f1bcf788253
Xinxin Peng <72983482+Xinxinatg@users.noreply.github.com>
2021-02-06 23:33:19 +1100

    Update compressed_transformer.py

--- models/compressed_transformer.py
+++ models/compressed_transformer.py
@@ -39,8 +39,8 @@ class CustomizedAttn(nn.Module):
     #    self.layernorm = nn.BatchNorm1d(d_model, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
         self.dropout = nn.Dropout(dropout)
         #to modify the first dimension according to the batch number
-        self.null_k = nn.Parameter(torch.zeros(10, 1, d_model))
-        self.null_v = nn.Parameter(torch.zeros(10, 1, d_model))
+        self.null_k = nn.Parameter(torch.zeros(1, 1, d_model))
+        self.null_v = nn.Parameter(torch.zeros(1, 1, d_model))
       
     def forward(self, k, q, value):   
         k=k.transpose(0,1)
