895f4736375f37d48bf4036272f92be5fa62ba10
Xinxin Peng <72983482+Xinxinatg@users.noreply.github.com>
2021-02-09 11:45:50 +1100

    Update crowd.py

--- datasets/crowd.py
+++ datasets/crowd.py
@@ -171,7 +171,9 @@ class Crowd_sh(Base):
     def __getitem__(self, item):
         img_path = self.im_list[item]
         name = os.path.basename(img_path).split('.')[0]
+        print('nameimage',name)
         gd_path = os.path.join(self.root_path, 'ground-truth', 'GT_{}.mat'.format(name))
+        print('gtpath',gd_path)
         img = Image.open(img_path).convert('RGB')
         keypoints = sio.loadmat(gd_path)['image_info'][0][0][0][0][0]
 
